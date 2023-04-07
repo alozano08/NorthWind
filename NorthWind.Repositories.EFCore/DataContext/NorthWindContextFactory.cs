96e1934c0c642bab97ae778f61371e81bfc0ff27
AlexisL <alozano0819@gmail.com>
2023-04-07 17:48:31 -0500

    Se crea proyecto, se crea NorthWind.Entities Enum-Exceptions-POCOEntities, se crea NorthWind.Repositories.EFCore DataContext-Migrations

---
+++ NorthWind.Repositories.EFCore/DataContext/NorthWindContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.Repositories.EFCore.DataContext
+{
+    class NorthWindContextFactory : IDesignTimeDbContextFactory<NorthWindContext>
+    {
+        public NorthWindContext CreateDbContext(string[] args)
+        {
+            var OptionBuilder = new DbContextOptionsBuilder<NorthWindContext>();
+            OptionBuilder.UseSqlServer(
+                "Server=(localdb)\\mssqllocaldb;database=NorthWindDb");
+            return new NorthWindContext(OptionBuilder.Options);
+        }
+    }
+}
