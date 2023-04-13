ce95fd70fe3c25e17db478a77daaa06b9dd03bbc
AlexisL <alozano0819@gmail.com>
2023-04-12 20:51:36 -0500

    Se crea pryecto WebApi - Controlador OrderController - se agregan servicios al Program

---
+++ NorthWind.WebApi/Controllers/OrderController.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using NorthWind.UseCases.CreateOrder;
+
+namespace NorthWind.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        readonly IMediator Mediator;
+        public OrderController(IMediator mediator) => 
+            Mediator = mediator;
+
+        [HttpPost("creater-order")]
+        public async Task<ActionResult<int>> CreateOrder(CreateOrderInputPort orderparams)
+        {
+            return await Mediator.Send(orderparams);
+        }
+    }
+}
